fb28c6028cfcaa30de59fbcc805cd0a94a0ad2af
isacvictorxs <146945793+isacvictorxs@users.noreply.github.com>
2024-08-08 07:45:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,22 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 
 const pergunta = [//abre a lista de objetos (itens)
     {//abre o iten
-        enunciado: "pergunta",
-        alternativa: [
-            "alternativa 1",
-            "alternativa 2"
-        ]
+        enunciado: "pergunta 1",
+        alternativa: ["alternativa 1", "alternativa 2" ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativa: ["alternativa 1", "alternativa 2" ]
+    },
+    {
+        enunciado: "pergunta 3",
+        alternativa: ["alternativa 1", "alternativa 2" ]
     }
+
 ]
+let posicao = 0;
+let perguntaAtual;
+
+function mostrapergunta (){
+    
+}
