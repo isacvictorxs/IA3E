d60d6704b467af9c6cc77c697736886a5ee5b2ff
isacvictorxs <146945793+isacvictorxs@users.noreply.github.com>
2024-10-03 07:49:22 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,14 +5,26 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [//abre a lista de objetos (itens)
     {//abre o item
-        enunciado:"Pergunta 1",
-        alternativas:["Alternativa 1","Alternativa 2"]
+        enunciado:"Você gosta da Inteligência Artificial?",
+        alternativas:[{
+            texto:"sim",
+            afirmacao:"afirmacao da alternativa1"
     },
     {
         enunciado:"Pergunta 2",
-        alternativas:["Alternativa 1","Alternativa 2"]
+        alternativas:[}
+        texto:"Quem sabe...",
+        afirmação:"Afirmação da alternativa 1"
     },
     {
+        texto:"talvez",
+        afirmação:"Afirmação da alternativa 2"
+    }
+    ] 
+},
+{
+    
+    
         enunciado:"Pergunta 3",
         alternativas:["Alternativa 1","Alternativa 2"]
     }
@@ -23,5 +35,13 @@ let perguntaAtual;
 function mostraPergunta(){
     perguntaAtual = perguntas[posicao];
     caixaPergunta.textContent = perguntaAtual.enunciado;
-    
+    mostraAlternativas();
 }
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativa.appendChild(botaoAlternativas);
+    }
+}
+mostraPergunta();
